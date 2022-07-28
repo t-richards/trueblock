d4041ae2b3fecdacece3354dc4260057a29e0736
Tom Richards <tom@tomrichards.net>
2022-07-27 22:14:53 -0400

    Working tests

--- src/background.test.ts
+++ src/background.test.ts
@@ -3,22 +3,21 @@ import { syncStorageToDnr } from './background'
 describe('sync storage to declarativeNetRequest', () => {
   describe('with no items in storage', () => {
     it('creates no DNR rules', async () => {
-      // Storage contains an empty object
-      const storageGet = jest.fn(async () => ({}))
-      Object.assign(global, { chrome: { storage: { sync: { get: storageGet } } } })
-      expect(storageGet).toHaveBeenCalledTimes(1)
-
-      // DNR rules are an empty array
-      const getDynamic = jest.fn(async () => ([]))
-      Object.assign(global, { chrome: { declarativeNetRequest: { getDynamicRules: getDynamic } } })
-      expect(getDynamic).toHaveBeenCalledTimes(1)
-
-      // This method should not be called
-      const updateDynamic = jest.fn()
-      Object.assign(global, { chrome: { declarativeNetRequest: { updateDynamicRules: updateDynamic } } })
+      const storageGet = jest.fn(async () => ({})) // Empty storage
+      const getDynamic = jest.fn(async () => ([])) // Empty DNR rules
+      const updateDynamic = jest.fn() // This function should not be called
+      Object.assign(global, { chrome: { 
+        storage: { sync: { get: storageGet } },
+        declarativeNetRequest: {
+          getDynamicRules: getDynamic,
+          updateDynamicRules: updateDynamic
+        }
+      } })
 
       await syncStorageToDnr()
 
+      expect(storageGet).toHaveBeenCalledTimes(1)
+      expect(getDynamic).toHaveBeenCalledTimes(1)
       expect(updateDynamic).not.toHaveBeenCalled()
     })
   })
