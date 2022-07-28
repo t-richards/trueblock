82471c7ee76f408f9126c466903f369ff0a0c4eb
Tom Richards <tom@tomrichards.net>
2022-07-27 22:22:57 -0400

    Move things

--- src/background.test.ts
+++ src/background.test.ts
@@ -3,22 +3,23 @@ import { syncStorageToDnr } from './background'
 describe('sync storage to declarativeNetRequest', () => {
   describe('with no items in storage', () => {
     it('creates no DNR rules', async () => {
-      const storageGet = jest.fn(async () => ({})) // Empty storage
-      const getDynamic = jest.fn(async () => ([])) // Empty DNR rules
-      const updateDynamic = jest.fn() // This function should not be called
-      Object.assign(global, { chrome: { 
-        storage: { sync: { get: storageGet } },
-        declarativeNetRequest: {
-          getDynamicRules: getDynamic,
-          updateDynamicRules: updateDynamic
+      Object.assign(global, {
+        chrome: {
+          storage: {
+            sync: {
+              get: jest.fn(async () => ({})) // Empty storage
+            }
+          },
+          declarativeNetRequest: {
+            getDynamicRules: jest.fn(async () => ([])), // Empty DNR rules
+            updateDynamicRules: jest.fn() // Should not be called
+          }
         }
-      } })
+      })
 
       await syncStorageToDnr()
 
-      expect(storageGet).toHaveBeenCalledTimes(1)
-      expect(getDynamic).toHaveBeenCalledTimes(1)
-      expect(updateDynamic).not.toHaveBeenCalled()
+      expect(chrome.declarativeNetRequest.updateDynamicRules).not.toHaveBeenCalled()
     })
   })
 })
