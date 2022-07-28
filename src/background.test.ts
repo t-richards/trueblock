79b321588aaa8bc327b0ad196b643e73a03e40d4
Tom Richards <tom@tomrichards.net>
2022-07-27 20:52:37 -0400

    More broken stuff

--- src/background.test.ts
+++ src/background.test.ts
@@ -1,17 +1,17 @@
 import { syncStorageToDnr } from './background'
 
-describe(syncStorageToDnr, () => {
+describe('sync storage to declarativeNetRequest', () => {
   describe('with no items in storage', () => {
     it('creates no DNR rules', async () => {
       // Storage contains an empty object
       const storageGet = jest.fn(async () => ({}))
       Object.assign(global, { chrome: { storage: { sync: { get: storageGet } } } })
-      expect(storageGet).toBeCalledTimes(1)
+      expect(storageGet).toHaveBeenCalledTimes(1)
 
       // DNR rules are an empty array
       const getDynamic = jest.fn(async () => ([]))
       Object.assign(global, { chrome: { declarativeNetRequest: { getDynamicRules: getDynamic } } })
-      expect(getDynamic).toBeCalledTimes(1)
+      expect(getDynamic).toHaveBeenCalledTimes(1)
 
       // This method should not be called
       const updateDynamic = jest.fn()
@@ -19,7 +19,7 @@ describe(syncStorageToDnr, () => {
 
       await syncStorageToDnr()
 
-      expect(updateDynamic).not.toBeCalled()
+      expect(updateDynamic).not.toHaveBeenCalled()
     })
   })
 })
