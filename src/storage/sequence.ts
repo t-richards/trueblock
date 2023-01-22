81edba973d8bcd3c18ada1a23f70ccbe629717f4
Tom Richards <tom@tomrichards.net>
2023-01-22 16:13:26 -0500

    Move esbuild, fix numbering

--- src/storage/sequence.ts
+++ src/storage/sequence.ts
@@ -3,8 +3,8 @@ export const ID_SEQUENCE_KEY = '__id_sequence'
 export async function getNextID() {
   const currentID = await chrome.storage.sync.get(ID_SEQUENCE_KEY)
   if (typeof currentID[ID_SEQUENCE_KEY] === 'undefined') {
-    await chrome.storage.sync.set({ [ID_SEQUENCE_KEY]: 1 })
-    return 1
+    await chrome.storage.sync.set({ [ID_SEQUENCE_KEY]: 0 })
+    return 0
   }
   const nextID = currentID[ID_SEQUENCE_KEY] + 1
   await chrome.storage.sync.set({ [ID_SEQUENCE_KEY]: nextID })
