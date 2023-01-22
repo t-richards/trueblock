6bafe13cd5951d83d8fb41d5c331ba682d4e7d3d
Tom Richards <tom@tomrichards.net>
2023-01-22 14:25:55 -0500

    Lint

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,5 @@
-import type { TestingLibraryMatchers } from '@types/testing-library__jest-dom/matchers';
+import type { TestingLibraryMatchers } from '@types/testing-library__jest-dom/matchers'
 
 declare module '@jest/expect' {
-  export interface Matchers<R = void, T = {}> extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+  export type Matchers<R = void> = TestingLibraryMatchers<typeof expect.stringContaining, R>
 }
